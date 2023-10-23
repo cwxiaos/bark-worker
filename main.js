3fc10950bf04477413e2a4de91e0ce38e517650f
cwxia0s <cwxia0s@com>
2023-10-23 20:24:39 +0800

    Add Some Functions

--- main.js
+++ main.js
@@ -4,16 +4,26 @@ export default {
   }
 }
 
+const isAllowNewDevice = false
+
 async function handleRequest(request, env, ctx) {
   const {searchParams, pathname} = new URL(request.url)
 
   switch(pathname){
     case "/register":{
-      return handler_register(searchParams)
+      return handler.register(env, searchParams)
       break
     }
     case "/ping":{
-      return handler_ping(searchParams)
+      return handler.ping(searchParams)
+      break
+    }
+    case "/healthz":{
+      return handler.healthz(searchParams)
+      break
+    }
+    case "/info":{
+      return handler.info(env, searchParams)
       break
     }
     case "/getClientInfo":{
@@ -24,59 +34,166 @@ async function handleRequest(request, env, ctx) {
       const Response_Access_Denied = {
         'message': 'Access Denied',
         'code': 500,
-        'timestamp': util_getTimestamp(),
+        'timestamp': util.getTimestamp(),
       }
       return new Response(JSON.stringify(Response_Access_Denied), {status: Response_Access_Denied.code})
     }
   }
 }
 
-async function handler_register(parameters){
-  const param_devicetoken = parameters.get('devicetoken')
-  var param_key = parameters.get('key')
+/**
+ * Class Handler
+ */
+class Handler {
+  constructor(){
+    this.version = "v2.0.0"
+    this.build = "Oct 23 2023"
+    this.arch = "js"
+    this.commit = "1"
+    this.devices = "0"
+  }
+  
+  async register(env, parameters){
+    const db = new Database(env)
 
-  var Response_register = {}
+    const param_devicetoken = parameters.get('devicetoken')
+    var param_key = parameters.get('key')
 
-  if(!param_devicetoken){
+    var Response_register = {}
+
+    if(!param_devicetoken){
+      Response_register = {
+        'message': 'device token is empty',
+        'code': 400,
+        'timestamp': util.getTimestamp(),
+      }
+
+      return new Response(JSON.stringify(Response_register), {status: Response_register.code})
+    }
+
+    if(!param_key){
+      if(isAllowNewDevice){
+        param_key = util.newShortUUID()
+        await db.saveDeviceTokenByKey(param_key, param_devicetoken)
+      }else{
+        Response_register = {
+          'message': "device registration failed: access denied",
+          'code': 500,
+        }
+
+        return new Response(JSON.stringify(Response_register), {status: Response_register.code})
+      }
+    }
+
     Response_register = {
-      'message': 'device token is empty',
-      'code': 400,
-      'timestamp': util_getTimestamp(),
+      'message': 'success',
+      'code': 200,
+      'timestamp': util.getTimestamp(),
+      'data': {
+        'key': param_key,
+        'device_key': param_key,
+        'device_token': param_devicetoken,
+      },
     }
 
     return new Response(JSON.stringify(Response_register), {status: Response_register.code})
   }
 
-  if(!param_key){
-    param_key = 'r5kEg9yprBLHmZ4aX8WGze'
+  async ping(parameters){
+    const Response_ping = {
+      'message': 'pong',
+      'code': 200,
+      'timestamp': util.getTimestamp(),
+    }
+
+    return new Response(JSON.stringify(Response_ping), {status: Response_ping.code})
   }
 
-  Response_register = {
-    'message': 'success',
-    'code': 200,
-    'timestamp': util_getTimestamp(),
-    'data': {
-      'key': param_key,
-      'device_key': param_key,
-      'device_token': param_devicetoken,
-    },
+  async healthz(parameters){
+    return new Response("ok")
   }
 
+  async info(env, parameters){
+    const db = new Database(env)
 
+    this.devices = await db.countAll()
 
-  return new Response(JSON.stringify(Response_register), {status: Response_register.code})
+    const Response_info = {
+      'version': this.version,
+      'build': this.build,
+      'arch': this.arch,
+      'commit': this.commit,
+      'devices': this.devices,
+    }
+
+    return new Response(JSON.stringify(Response_info), {status: 200})
+  }
 }
 
-async function handler_ping(parameters){
-  const Response_ping = {
-    'message': 'pong',
-    'code': 200,
-    'timestamp': util_getTimestamp(),
-  }
+const handler = new Handler()
 
-  return new Response(JSON.stringify(Response_ping), {status: Response_ping.code})
+/**
+ * Class Database
+ */
+class Database {
+  constructor(env){
+    const kvStorage = env.database
+
+    this.countAll = async () => {
+      const count = (await kvStorage.list()).keys.length
+      return count
+    }
+
+    this.deviceTokenByKey = async (key) => {
+      const deviceToken = await kvStorage.get(key)
+      return deviceToken
+    }
+
+    this.saveDeviceTokenByKey = async (key, token) => {
+      const deviceToken = await kvStorage.put(key, token)
+      return await deviceToken
+    }
+  }
 }
 
+// const database = new Database()
+
+/**
+ * Class Util
+ */
+class Util {
+  constructor(){}
+  getTimestamp(){
+    return Math.floor(Date.now() / 1000)
+  }
+
+  base64ToArrayBuffer(base64) {
+    const binaryString = atob(base64);
+    const length = binaryString.length;
+    const buffer = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+      buffer[i] = binaryString.charCodeAt(i);
+    }
+    return buffer;
+  }
+
+  newShortUUID(){
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const length = 22; //UUID长度
+    let customUUID = '';
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      customUUID += characters[randomIndex];
+    }
+
+    return customUUID;
+  }
+}
+
+const util = new Util()
+
+/* -------------------------------------------------------------------------------------------------------------------------- */
 async function apns_send(){
   //return new Response(JSON.stringify(Status_Access_Denied), { status: Status_Access_Denied.code})
 
@@ -97,7 +214,7 @@ async function apns_send(){
     .replace(/\s/g, ''); // 移除所有空白字符
 
     // 解码 Base64 编码的私钥为 ArrayBuffer
-    const privateKeyArrayBuffer = base64ToArrayBuffer(privateKeyPEM);
+    const privateKeyArrayBuffer = util.base64ToArrayBuffer(privateKeyPEM);
     // return new Response(privateKeyArrayBuffer)
 
     // 解析私钥
@@ -112,7 +229,7 @@ async function apns_send(){
       ['sign']
     );
 
-    const DEVICE_TOKEN = '123';
+    const DEVICE_TOKEN = '';
     const TEAM_ID = '5U8LBRXG3A';
     const AUTH_KEY_ID = 'LH4T9V5U4R';
     const TOPIC = 'me.fin.bark';
@@ -164,18 +281,4 @@ async function apns_send(){
   }
 
   return new Response("flag");
-}
-
-function base64ToArrayBuffer(base64) {
-  const binaryString = atob(base64);
-  const length = binaryString.length;
-  const buffer = new Uint8Array(length);
-  for (let i = 0; i < length; i++) {
-    buffer[i] = binaryString.charCodeAt(i);
-  }
-  return buffer;
-}
-
-function util_getTimestamp(){
-  return Math.floor(Date.now() / 1000)
 }
